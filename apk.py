af63f312bc17bfabaf6c3fe49541e20bf94fad28
growwweb <growwweb@gmail.com>
2022-10-17 12:09:42 +0300

    Initial commit

--- apk.py
+++ apk.py
@@ -7,7 +7,7 @@ numbers of small squares. You should see a black canvas with buttons and a
 label at the bottom. Pressing the buttons adds small colored squares to the
 canvas.
 
-test
+eryreyery
 
 '''
 
